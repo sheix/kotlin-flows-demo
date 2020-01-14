e8007a1b6b3829f5948516d8a4c5888619c1c8a2
Anton Sheihman <anton.sheihman@tufin.com>
2020-01-14 11:34:34 +0200

    Add a code for demo

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,93 @@
+package info.sheix.flows
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import java.time.Instant
+import kotlin.system.measureTimeMillis
+
+@InternalCoroutinesApi
+@ExperimentalCoroutinesApi
+fun main() = runBlocking<Unit> {
+    println("-FLOW DEMO--")
+    flowDemo()
+    println("------------")
+    println("-ASYNC DEMO-")
+    asyncDemo()
+    println("------------")
+    println("-CANCEL DMO-")
+    cancelDemo()
+}
+
+
+private fun asyncDemo() = runBlocking {
+    val startTime = Instant.now()
+
+    val longInt = async { longMap(11) }
+    val anotherLongInt = async { longMap(12) }
+
+    val longSum = longInt.await() + anotherLongInt.await()
+
+    val time = Instant.now().toEpochMilli() - startTime.toEpochMilli()
+
+    println("Here is the answer ($longSum) and it took $time milliseconds to get there")
+
+    assert(time < 1100)
+    assert(time > 1000)
+}
+
+@InternalCoroutinesApi
+@ExperimentalCoroutinesApi
+private suspend fun flowDemo() {
+    val flows = mapOf(
+        "simple" to createFlow(),
+        "buffered" to createFlow(withBuffer = true),
+        "distinct" to createFlow(withDistinct = true),
+        "both" to createFlow(true, true)
+    )
+
+    flows.forEach { flow ->
+
+        println("Flow (${flow.key}) created, will run it now")
+
+        println("it takes ${measureTimeMillis {
+            runBlocking {
+                flow.value.collect { println(it) }
+            }
+        }} milliseconds")
+        //program cancellation friendly
+        yield()
+
+        println("Done with the (${flow.key}) flow")
+    }
+}
+
+@ExperimentalCoroutinesApi
+fun createFlow(withBuffer: Boolean = false, withDistinct: Boolean = false): Flow<Int> {
+    var flow = listOf(1, 2, 2, 2, 2, 2, 3, 4, 5, 2)
+        .asFlow()
+        .onCompletion { println("Flow completed") }
+    if (withBuffer) flow = flow.buffer(1)
+    if (withDistinct) flow = flow.distinctUntilChanged()
+
+    return flow.map { longMap(it) }
+}
+
+
+suspend fun longMap(i: Int): Int {
+    delay(1000)
+    return i * i
+}
+
+@InternalCoroutinesApi
+private suspend fun CoroutineScope.cancelDemo() {
+    val job = launch { flowDemo() }
+    delay(200)
+    job.cancel("KillAllHumans!")
+    println("Cancelling job now")
+}
+
+
+
+
+
+
---
+++ src/test/kotlin/AsyncDemo.kt
@@ -0,0 +1,59 @@
+import info.sheix.flows.createFlow
+import info.sheix.flows.longMap
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.reactor.asFlux
+import kotlinx.coroutines.test.runBlockingTest
+import org.amshove.kluent.`should be greater than`
+import org.amshove.kluent.`should be less than`
+import org.amshove.kluent.`should equal`
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.test
+import java.time.Instant.now
+
+
+@ExperimentalCoroutinesApi
+class AsyncDemo {
+
+    @Test
+    fun `GIVEN - simple async await WHEN - run in blocking test THEN - delays are skipped`() = runBlockingTest {
+        val startTime = now()
+        val longInt = async { longMap(11) }
+        val anotherLongInt = async { longMap(12) }
+
+        val longSum = longInt.await() + anotherLongInt.await()
+
+        longSum `should equal` 265
+
+        val time = now().toEpochMilli() - startTime.toEpochMilli()
+
+        time `should be greater than` 1
+        time `should be less than` 100
+    }
+
+
+    @Test
+    fun `GIVEN - blocking function WHEN - is run THEN - test works`() = runBlockingTest {
+        val number = 10
+        val result = longMap(number)
+
+        result `should equal` 100
+    }
+
+    @Test
+    fun `GIVEN - flow WHEN - converted to flux and subscribed THEN - runs as expected`() {
+        createFlow(withBuffer = true, withDistinct = false).asFlux().test()
+            .expectNext(1)
+            .expectNext(4)
+            .expectNext(4)
+            .expectNext(4)
+            .expectNext(4)
+            .expectNext(4)
+            .expectNext(9)
+            .expectNext(16)
+            .expectNext(25)
+            .expectNext(4)
+            .verifyComplete()
+    }
+
+}
